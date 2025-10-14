ba5b56b46395454f4249e684929f152c594e9633
byronwang2005 <hzwx2005@qq.com>
2025-10-14 15:36:25 +0800

    upgrade auto-mode

--- ccc_attendance.py
+++ ccc_attendance.py
@@ -2,59 +2,50 @@ import qrcode
 import matplotlib.pyplot as plt
 import datetime
 
-
 def datetime_to_timestamp(year, month, day, hour, minute):
     """将年月日时分转换为毫秒级时间戳"""
     dt = datetime.datetime(year, month, day, hour, minute)
     return int(dt.timestamp() * 1000)
 
-
 def erweima(url):
     qr = qrcode.QRCode(
-        version=1,  # 二维码的版本，范围从1到40，数字越大，二维码的格子数越多，可存储的信息也越多
-        error_correction=qrcode.constants.ERROR_CORRECT_L,  # 纠错级别，L表示约7%的纠错能力
-        box_size=10,  # 每个小格子包含的像素数
-        border=4,  # 二维码四周留白的格子数
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
     )
     qr.add_data(url)
     qr.make(fit=True)
     img = qr.make_image(fill_color="#FFFFFF", back_color="#10263B") #诺丁蓝色
     plt.imshow(img)
-    plt.axis('off')  # 不显示坐标轴
+    plt.axis('off')
     plt.show()
-    # 保存二维码图片
     img.save("qrcode.png")
 
-
-#main
 print("使用教学：")
-print("把中国文化课主界面在手机浏览器中打开，长按“查看详情”按钮复制链接，在下方粘贴。")
+print("把中国文化课主界面(https://ccc.nottingham.edu.cn/study/)在手机浏览器中打开，长按欲签到课程的“查看详情”按钮复制链接，在下方粘贴。")
+print("您所复制链接的格式应为https://ccc.nottingham.edu.cn/study/home/details?scheduleId=xxxx")
 url=input("请输入链接：")
-
-funct=int(input("自动模式（1）/手动模式（2）\n请输入数字（默认自动模式）："))
-if funct==2:
-    print("手动签到已启用！\n请输入签到时间（24小时制）:")
-    year = int(input("年: "))
-    month = int(input("月: "))
-    day = int(input("日: "))
-    hour = int(input("时: "))
-    minute = int(input("分: "))
-    timestamp = datetime_to_timestamp(year, month, day, hour, minute)
-    if "https://ccc.nottingham.edu.cn/study/home/details?" in url:
-        id = url[52:]
+if "https://ccc.nottingham.edu.cn/study/home/details?" in url:
+    id = url[52:]
+    funct=int(input("自动模式（1）/手动模式（2）\n请输入数字（默认自动模式）："))
+    if funct==2:
+        print("手动签到已启用！\n请输入签到时间（24小时制）:")
+        year = int(input("年: "))
+        month = int(input("月: "))
+        day = int(input("日: "))
+        hour = int(input("时: "))
+        minute = int(input("分: "))
+        timestamp = datetime_to_timestamp(year, month, day, hour, minute)
         attendance = f"https://ccc.nottingham.edu.cn/study/attendance?scheduleId={id}&time={timestamp}"
-        print(f"生成的签到链接: {attendance}")
-        erweima(attendance)
     else:
-        print("弄错了！不是合法的ccc链接！！！")
-else:#固定2030时间戳
-    if "https://ccc.nottingham.edu.cn/study/home/details?" in url:
-        id=url[52:]
-        attendance="https://ccc.nottingham.edu.cn/study/attendance?scheduleId="+id+"&time=1893456000"
-        print(attendance)
-        erweima(attendance)
-    else:
-        print("弄错了！不是合法的ccc链接！！！")
+        print("自动签到已启用！\n正在读取系统本地时间......")
+        local_ms_timestamp = int(datetime.datetime.now().timestamp() * 1000)
+        attendance = f"https://ccc.nottingham.edu.cn/study/attendance?scheduleId={id}&time={local_ms_timestamp}"
+    print(f"生成的签到链接: {attendance}")
+    erweima(attendance)
+else:
+    print("链接格式有误！请重试一次哦～")
 
 
 
