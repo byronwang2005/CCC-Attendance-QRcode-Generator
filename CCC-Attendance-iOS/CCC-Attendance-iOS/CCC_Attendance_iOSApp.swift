65abd8a7f59b73f18282e941d45ef62b8dcbcdaf
byronwang2005 <hzwx2005@qq.com>
2025-11-24 16:20:58 +0800

    Build iOS application

---
+++ CCC-Attendance-iOS/CCC-Attendance-iOS/CCC_Attendance_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCC_Attendance_iOSApp.swift
+//  CCC-Attendance-iOS
+//
+//  Created by MacBook on 2025/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CCC_Attendance_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
